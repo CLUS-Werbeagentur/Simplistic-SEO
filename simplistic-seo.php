ba7dc43a81958945e70a19c0e94fa9f9faf14f83
Kevin Walker <mail@kevinwalker.ch>
2017-03-28 17:11:00 +0200

    👽

--- simplistic-seo.php
+++ simplistic-seo.php
@@ -1,75 +1,124 @@
 <?php
-
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://walkeezy.ch
- * @since             1.0.0
- * @package           Simplistic_Seo
- *
- * @wordpress-plugin
- * Plugin Name:       Simplistic SEO
- * Plugin URI:        http://walkeezy.ch/simplistic-seo
- * Description:       All you need regarding SEO on a WordPress website.
- * Version:           1.0.0
- * Author:            Kevin Walker
- * Author URI:        http://walkeezy.ch
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       simplistic-seo
- * Domain Path:       /languages
+ * Plugin Name: Simplistic SEO
+ * Plugin URI: http://walkeezy.ch
+ * Description: All SEO you will ever need.
+ * Version: 1.0.0
+ * Author: Kevin Walker
+ * Author URI: http://walkeezy.ch
+ * License: GPL2
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+ // ADD METATAGS TO THE HEAD
+ //-----------------------------------------------------------------------
+
+function sseo_metatags() {
+
+	global $post;
+	$description = get_post_meta($post->ID, '_sseo_metadescription', true); ?>
+
+<meta type="description" content="<?php echo $description; ?>" />
+
+<?php }
+
+add_filter( 'wp_head', 'sseo_metatags', 1 );
+
+
+// SETTINGS PAGE
+//-----------------------------------------------------------------------
+
+class settingsPage {
+
+	function __construct() {
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
+	}
+
+	function admin_menu() {
+		add_options_page(
+			'Page Title',
+			'SEO Einstellungen',
+			'manage_options',
+			'options_page_slug',
+			array(
+				$this,
+				'settings_page'
+			)
+		);
+	}
+
+	function register_settings() {
+		register_setting( 'sseo_settings', 'sseo_test' );
+	}
+
+	function settings_page() { ?>
+
+		<div class="wrap">
+			<h1><?php _e( 'SEO Einstellungen', 'simplistic-seo' ) ?></h1>
+
+			<form method="post" action="options.php">
+				<?php settings_fields( 'sseo_settings' ); ?>
+				<?php do_settings_sections( 'sseo_settings' ); ?>
+				<table class="form-table">
+					<tr valign="top">
+						<th scope="row">Test</th>
+						<td><input type="text" name="sseo_test" value="<?php echo esc_attr( get_option('sseo_test') ); ?>" /></td>
+					</tr>
+				</table>
+				<?php submit_button(); ?>
+			</form>
+		</div>
+
+	<?php }
 }
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-simplistic-seo-activator.php
- */
-function activate_simplistic_seo() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-simplistic-seo-activator.php';
-	Simplistic_Seo_Activator::activate();
+new settingsPage;
+
+// METABOX
+//-----------------------------------------------------------------------
+
+class metaBox {
+
+	function __construct() {
+		add_action( 'add_meta_boxes', array( $this, 'register_metabox' ) );
+		add_action( 'save_post', array( $this, 'save_metabox' ) );
+	}
+
+	function register_metabox() {
+		add_meta_box( 'sseo-metabox', __( 'SEO Einstellungen', 'simplistic-seo' ), array( $this, 'render_metabox' ), 'page' );
+	}
+
+	function render_metabox() {
+
+		global $post;
+    $values = get_post_custom( $post->ID );
+		$metadescription = isset( $values['_sseo_metadescription'] ) ? $values['_sseo_metadescription'][0] : '';
+
+		wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' ); ?>
+
+		<p class="post-attributes-label-wrapper"><label class="post-attributes-label" for="sseo-metadescription">Metadescription</label></p>
+		<textarea name="metadescription" class="postbox" id="metadescription"><?php echo $metadescription; ?></textarea>
+
+	<?php }
+
+	function save_metabox($post_id) {
+
+		// Bail if we're doing an auto save
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+
+    // if our nonce isn't there, or we can't verify it, bail
+    if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'my_meta_box_nonce' ) ) return;
+
+    // if our current user can't edit this post, bail
+    if( !current_user_can( 'edit_post' ) ) return;
+
+    if( isset( $_POST['metadescription'] ) )
+			update_post_meta( $post_id, '_sseo_metadescription', esc_attr( $_POST['metadescription'] ) );
+
+	}
+
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-simplistic-seo-deactivator.php
- */
-function deactivate_simplistic_seo() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-simplistic-seo-deactivator.php';
-	Simplistic_Seo_Deactivator::deactivate();
-}
+new metaBox;
 
-register_activation_hook( __FILE__, 'activate_simplistic_seo' );
-register_deactivation_hook( __FILE__, 'deactivate_simplistic_seo' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-simplistic-seo.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_simplistic_seo() {
-
-	$plugin = new Simplistic_Seo();
-	$plugin->run();
-
-}
-run_simplistic_seo();
+?>
